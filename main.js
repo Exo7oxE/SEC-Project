bc7ac2464cab2e51af6c3b0fafc3a8cf1283e8d8
Exo7oxE <97412640+Exo7oxE@users.noreply.github.com>
2024-01-08 14:58:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-var btn = document.querySelector(".button");
-let num = 0;
 
-function motionClicked() {
- num++;
- btn.classList.add("anim");
- setTimeout(() -> {
-  btn.classList.remove("anim");
- }, 1000);
-}
