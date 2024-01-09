44b17736816f766ce0182b82de5146a072ea83d0
Exo7oxE <97412640+Exo7oxE@users.noreply.github.com>
2024-01-09 11:29:46 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function informCat() {
 	document.getElementById('no motion').hidden = true;
 	document.getElementById('motion').hidden = false;
+	alert('whole lotta motion');
 	setTimeout(() => {
 		document.getElementById('no motion').hidden = false;
 		document.getElementById('motion').hidden = true;
